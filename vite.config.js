7740aa1fa13d52d0520d42ceb1f52fc9525ab728
casluftw <cyberlukinhas@gmail.com>
2023-09-07 11:16:11 -0300

    primeiro commit do projeto do intensivao de javascript da hashtag

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazineIntensivao/",
+  base: "/magazineIntensivao_/",
   plugins: [],
   resolve: {
     /*something*/
