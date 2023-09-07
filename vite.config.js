29439bfd9953392a59f918ddbb44b93937bafbed
casluftw <ilucasmcpvp@gmail.com>
2023-09-07 11:32:16 -0300

    Primeiro commit do Projeto do Intensivao de JavaScript da HashTag

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/magazineIntensivao_/",
+  base: "/magazineIntensivao/",
   plugins: [],
   resolve: {
     /*something*/
